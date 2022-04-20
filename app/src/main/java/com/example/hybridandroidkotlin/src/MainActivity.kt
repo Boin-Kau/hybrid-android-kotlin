df6855c16c86555cce8e3931073d33cb6fa95c9b
Boin Kim <charles1236@kau.kr>
2022-04-20 20:29:34 +0900

    Add: ViewBinding 및 BaseActivity 세팅

--- app/src/main/java/com/example/hybridandroidkotlin/src/MainActivity.kt
+++ app/src/main/java/com/example/hybridandroidkotlin/src/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.hybridandroidkotlin.src
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hybridandroidkotlin.R
+import com.example.hybridandroidkotlin.config.BaseActivity
+import com.example.hybridandroidkotlin.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
+
 }
